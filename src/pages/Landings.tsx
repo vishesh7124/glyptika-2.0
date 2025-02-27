dcf16ab409be2ee0e3092ec4749182fa0ed3d3b0
vishesh7124 <87852777+vishesh7124@users.noreply.github.com>
2025-02-27 14:25:01 +0530

    fixed unused var

--- src/pages/Landings.tsx
+++ src/pages/Landings.tsx
@@ -4,25 +4,25 @@
 // // import React from "react";
 // import { AuroraBackground } from "@/components/ui/aurora-background";
 
-import { Compare } from "@/components/ui/compare";
+// import { Compare } from "@/components/ui/compare";
 import LandingVideo from "@/components/LandingVid";
 import { MarqueeDemoVertical } from "@/components/AnimatedGallery";
 import { Button } from "@/components/ui/button";
 
-const CompareDemo = () => {
-  return (
-    <div className="h-full w-full flex flex-col justify-center items-center">
-      <Compare
-        firstImage="https://assets.aceternity.com/code-problem.png"
-        secondImage="https://assets.aceternity.com/code-solution.png"
-        firstImageClassName="object-cover object-left-top"
-        secondImageClassname="object-cover object-left-top"
-        className="h-[250px] w-[200px] md:h-[500px] md:w-[500px]"
-        slideMode="drag"
-      />
-    </div>
-  );
-};
+// const CompareDemo = () => {
+//   return (
+//     <div className="h-full w-full flex flex-col justify-center items-center">
+//       <Compare
+//         firstImage="https://assets.aceternity.com/code-problem.png"
+//         secondImage="https://assets.aceternity.com/code-solution.png"
+//         firstImageClassName="object-cover object-left-top"
+//         secondImageClassname="object-cover object-left-top"
+//         className="h-[250px] w-[200px] md:h-[500px] md:w-[500px]"
+//         slideMode="drag"
+//       />
+//     </div>
+//   );
+// };
 
 export default function Landings() {
   return (
