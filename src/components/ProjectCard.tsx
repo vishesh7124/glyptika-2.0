eb8358897af81a70e67bdd2e3debb361de602ec8
vishesh7124 <87852777+vishesh7124@users.noreply.github.com>
2025-03-15 15:10:26 +0530

    making grid dynamic

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -12,21 +12,23 @@ import vid1 from '../assets/videos/AlphaTreadShoes.mp4'
 import vid2 from '../assets/videos/Samsung.mp4'
 
 export default function ProjectCard() {
+  console.log(items)
+
   return (
-    <BentoGrid className="w-screen  md:auto-rows-[18rem]">
+    <BentoGrid className="w-screen  md:auto-rows-[22rem]">
       {items.map((item, i) => (
         <BentoGridItem
           key={i}
           title={"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, tempora. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, tempora. "}
           description={<Button className="bg-[#5EC2B5] p-4 rounded-3xl  " >Know More</Button>}
           header={<video
-            src={i%2==0 ? vid1:vid2}
+            src={(i+1)%2==0 ? vid1:"https://res.cloudinary.com/duifdyecp/video/upload/v1741978814/dpbbvhgjfhu3umuxvizc.mp4"}
             className="w-full h-full rounded-md object-fill  "
             autoPlay
             loop
             muted
           />}
-          className={item.className}
+          className={`md:${gridPattern[i%gridPattern.length]}`}
           icon={item.icon}
         />
       ))}
@@ -36,6 +38,9 @@ export default function ProjectCard() {
 const Skeleton = () => (
   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl   dark:bg-dot-white/[0.2] bg-dot-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white,transparent)]  border border-transparent dark:border-white/[0.2] bg-neutral-100 dark:bg-black"></div>
 );
+
+const gridPattern = ["col-span-2", "col-span-1",  "col-span-1", "col-span-2","col-span-3"];
+
 const items = [
   {
     title: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, tempora. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, tempora. ",
@@ -51,25 +56,57 @@ const items = [
     icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
   },
   {
-    title: "The Digital Revolution",
-    description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
+    title: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, tempora. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, tempora. ",
+    description: <Button className="bg-[#5EC2B5] p-4 rounded-3xl  " >Know More</Button>    ,
+    header:       <video
+    src={vid1}
+    className="w-full h-full rounded-md object-fill  "
+    autoPlay
+    loop
+    muted
+  />,
     className: "md:col-span-1",
-    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
   },
   {
-    title: "The Art of Design",
-    description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton />,
+    title: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, tempora. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, tempora. ",
+    description: <Button className="bg-[#5EC2B5] p-4 rounded-3xl  " >Know More</Button>    ,
+    header:       <video
+    src={vid1}
+    className="w-full h-full rounded-md object-fill  "
+    autoPlay
+    loop
+    muted
+  />,
     className: "md:col-span-1",
-    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
   },
   {
-    title: "The Power of Communication",
-    description:
-      "Understand the impact of effective communication in our lives.",
-    header: <Skeleton />,
+    title: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, tempora. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, tempora. ",
+    description: <Button className="bg-[#5EC2B5] p-4 rounded-3xl  " >Know More</Button>    ,
+    header:       <video
+    src={vid1}
+    className="w-full h-full rounded-md object-fill  "
+    autoPlay
+    loop
+    muted
+  />,
     className: "md:col-span-2",
-    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
   },
+  {
+    title: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, tempora. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, tempora. ",
+    description: <Button className="bg-[#5EC2B5] p-4 rounded-3xl  " >Know More</Button>    ,
+    header:       <video
+    src={vid1}
+    className="w-full h-full rounded-md object-fill  "
+    autoPlay
+    loop
+    muted
+  />,
+    className: "md:col-span-3 ",
+    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+  },
+
+
 ];
